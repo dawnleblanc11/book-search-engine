62a8e205fd9e46ef8a887828566fd387a31fce93
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-02-22 14:22:24 -0500

    implement apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+//import Apollo Server
+const { ApolloServer} = require('apollo-server-express');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { authMiddleware } = require('./utils/auth');
+let server = "";
+
+//create a new Apollo Seriver and pass in schema data
+async function startServer() {
+    server = new ApolloServer({
+       typeDefs,
+       resolvers,
+       context: authMiddleware
+    });
+    await server.startsWith();
+    // integrate Apoloo server with the express function as middleware
+}
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
